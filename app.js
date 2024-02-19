8bf7f846e8ca10040e0ea8c1fe73dc98004473c8
TurNyak <124363255+TurNyak@users.noreply.github.com>
2024-02-19 11:30:28 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const classList = [{id: "ID",name:"NAME",lastN:"LASTNAME",pick:"PICK"}]
-classList.push({name:"Filip", lastN:"Bugaj", pick:false})
-classList.push({name:"Jakub", lastN:"Freier", pick:false})
-classList.push({name:"Oskar", lastN:"Juzefowski", pick:false})
-classList.push({name:"Sebastian", lastN:"Latusek", pick:false})
-classList.push({name:"Oleksandr", lastN:"Koval", pick:false})
-classList.push({name:"Maksymilian", lastN:"Gdawiec", pick:false})
-classList.push({name:"Mariusz", lastN:"Mann", pick:false})
-classList.push({name:"Oliwer", lastN:"Loson", pick:false})
-classList.push({name:"Adrian", lastN:"Hadasik", pick:false})
-classList.push({name:"Jakub", lastN:"Pajak", pick:false})
-classList.push({name:"Domenik", lastN:"Kijko", pick:false})
-classList.push({name:"Evgen", lastN:"Malenko", pick:false})
-classList.push({name:"Igor", lastN:"Olszanowic", pick:false})
-classList.push({name:"Mateusz", lastN:"Szeliec", pick:false})
-classList.push({name:"Adrian", lastN:"Szmandra", pick:false})
-classList.push({name:"Maksym", lastN:"Voinerovskyj", pick:false})
+//To quickly clear a participant who is not participating at this moment, you can add //classList.push({name:"Filip", lastN:"Bugaj", pick: ""})
+classList.push({name:"Filip", lastN:"Bugaj", pick: ""})
+classList.push({name:"Jakub", lastN:"Freier", pick: ""})
+classList.push({name:"Oskar", lastN:"Juzefowski", pick: ""})
+classList.push({name:"Sebastian", lastN:"Latusek", pick: ""})
+classList.push({name:"Oleksandr", lastN:"Koval", pick: ""})
+classList.push({name:"Maksymilian", lastN:"Gdawiec", pick: ""})
+classList.push({name:"Mariusz", lastN:"Mann", pick: ""})
+classList.push({name:"Oliwer", lastN:"Loson", pick: ""})
+classList.push({name:"Adrian", lastN:"Hadasik", pick: ""})
+classList.push({name:"Jakub", lastN:"Pajak", pick: ""})
+classList.push({name:"Domenik", lastN:"Kijko", pick: ""})
+classList.push({name:"Evgen", lastN:"Malenko", pick: ""})
+classList.push({name:"Igor", lastN:"Olszanowic", pick: ""})
+classList.push({name:"Mateusz", lastN:"Szeliec", pick: ""})
+classList.push({name:"Adrian", lastN:"Szmandra", pick: ""})
+classList.push({name:"Maksym", lastN:"Voinerovskyj", pick: ""})
 
 
 //get id for all 
@@ -22,14 +23,20 @@ for(let i = 1; i < classList.length;i++){
     classList[i].id = i
 }
 
-//const sort = classList.map((x) => x = {id: x.id,name: x.name,lastN: x.lastN})
-//console.log(sort)
-
+//creating the first informative table of participants
 var firstTable = document.querySelector('#firstTable')
 createTable(firstTable, classList.map((x) => x = {id: x.id,name: x.name,lastN: x.lastN}))
 
+//ready-made table selected for each participant
 var firstTablePick = document.querySelector('#firstTablePick')
-createTable(firstTablePick, classList.map((x) => x = {pick: x.pick}))
+let pi = resultTable(pairOrphanless(classList.map((x) => x = [x.id]).splice(1,classList.length)))
+createTable(firstTablePick, pi)
+
+
+function pickReady(){   
+    location. reload()
+}
+
 function createTable(parent, objArray){
     var table = document.createElement('table')
 
@@ -47,11 +54,65 @@ function createTable(parent, objArray){
     parent.appendChild(table)
 }
 
-//function for random 
 
 
 
+//we form the correct table for the algorithm for creating tables on the page
+function resultTable(te){
+    const r = [{name:"NAME", lastN: "LAST NAME"}] 
+    for(const key of Object.keys(te)){
+        r.push({name: classList[te[key]].name,lastN: classList[te[key]].lastN})
+    }
+    return r
+}
 
 
 
+//console.log(pairOrphanless(classList.map((x) => x = [x.id]).splice(1,classList.length)));
+//references from Sebastian 
 
+
+function pairOrphanless(Els) {
+    let pairs = {};
+    for (const key of Els)
+        pairs[key] = null;
+
+    let current_key = Els[0];
+    
+    while (true) {
+        // if chosen paired, we don't want to cycle - we want to start from next to pair
+        if (pairs[current_key] != null) {
+            for (const key of Object.keys(pairs)) {
+                if (pairs[key] == null) {
+                    current_key = key;
+                    break;
+                }
+            }
+        }
+
+        let possible_for_pair = [...Els];
+
+        // cannot pair itself
+        possible_for_pair.splice(possible_for_pair.indexOf(current_key), 1);
+
+        // delete already paired
+        possible_for_pair = possible_for_pair.filter((p) => !Object.values(pairs).includes(p));
+
+        // if have choice between two, choose unpaired to avoid orphan
+        if (possible_for_pair.length == 2) {
+            possible_for_pair = possible_for_pair.filter((p) => pairs[p] == null);
+        }
+
+        //console.log(current_key, possible_for_pair);
+        if (possible_for_pair.length == 0) break;
+    
+        let to_pair = possible_for_pair[
+            Math.floor(Math.random()*possible_for_pair.length)
+        ];
+
+        pairs[current_key] = to_pair;
+        current_key = to_pair;
+    }
+
+    return pairs;
+}
